70bcf7e00d046d3eb2aa4be1a9c7e5bc23907959
Alena <xellatroy@gmail.com>
2022-09-19 22:21:30 +0300

    Начала работу над открытием меню

--- scripts.js
+++ scripts.js
@@ -1,5 +1,14 @@
 const btnMenuOpen = document.getElementById("btn_open");
+const main = document.getElementById("main");
+const menu = document.getElementById("menu");
+const btnMenuClose = document.getElementById("btn_close")
 
 btnMenuOpen.addEventListener("click", function() {
-    
-})
+    main.classList.add("main_open");
+    menu.classList.add("menu_open");
+});
+
+btnMenuClose.addEventListener("click", function() {
+    main.classList.remove("main_open");
+    menu.classList.remove("menu_open");
+});
